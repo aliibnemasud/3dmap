3e56553bb2e15b6ab69441b6ccf336bc7ccce285
aliibnemasud <mdmasudrony@gmail.com>
2023-03-28 12:56:14 +0600

    code divided by other files

---
+++ globe.js
@@ -0,0 +1,70 @@
+const colorScale = d3.scaleSequentialSqrt(d3.interpolateYlOrRd);
+const getVal = (feat) => feat.properties.GDP_MD_EST / Math.max(1e5, feat.properties.POP_EST);
+fetch("finalUpdatedAdminCountryData4.json")
+  .then((res) => res.json())
+  .then((countries) => {
+    const maxVal = Math.max(...countries.features.map(getVal));
+    colorScale.domain([0, maxVal]);
+    const world = Globe()
+      .globeImageUrl("//unpkg.com/three-globe/example/img/earth-night.jpg")
+      .backgroundImageUrl("//unpkg.com/three-globe/example/img/night-sky.png")
+      .lineHoverPrecision(0)
+      .polygonsData(countries.features.filter((d) => d.properties.ISO_A2 !== "AQ"))
+      .polygonAltitude(0.06)
+      .polygonCapColor((feat) => feat?.properties?.color) // polygon color coming from color property
+      .polygonSideColor(() => "rgba(0, 100, 0, 0.15)") // ground color
+      .polygonStrokeColor(() => "#111")
+      .onPolygonClick(({ properties: d }) => {
+        let committeesDetailsData = [];
+        fetch("UNTrendyBodyAndRegionalOnes.json")
+          .then((res) => res.json())
+          .then((committeesDetails) => {
+            let committees = d.UNTreatyBody;
+            let institutions = d.regionalHumanRightsMechanism;
+            let UNTreatyBodyData = committeesDetails?.UNTrendyBody?.filter(function (item) {
+              return committees.indexOf(item?.committee) !== -1;
+            });
+            
+            let regionalHumanRightsMechanismData = committeesDetails?.regionalOnes?.filter(function (item) {
+              return institutions.indexOf(item?.institution) !== -1;
+            });
+
+            let UNTreatyBody = [
+              `<div><h4>UN Treaty Body:</h4><ul>${UNTreatyBodyData?.map((un) => {
+                return `<div>
+                              <li><p>${un?.abbreviations}</p></li>
+                              <p><a target="_blank" href=${un?.individualComplaintLink}>Individual Complaint</a></p>
+                              </div>`;
+              }).join(" ")} </div>`,
+            ];
+
+            let RegionalHuman = [
+              `<h4 style="margin: 0;">Regional Human Rights Mechanism:</h4> <ul>${regionalHumanRightsMechanismData?.map((un) => {
+                return `<li><a target="_blank" href=${un?.IndividualComplaint}>${un?.abbreviations}</a></li>`;
+              })} </div>`,
+            ];
+
+            showPopup(`
+                  <div class="top-part">
+                    <h2 style="margin: 0;">${d.BRK_NAME}</h2>
+                    <button  onclick="hidePopup()" class="closeBtn"><i class="fa-sharp fa-solid fa-xmark"></i></button>
+                  </div>
+                  ${
+                    d?.UNTreatyBody[0]?.length === 0
+                      ? `<h4>UN Treaty Body:</h4><p>Unfortunately in ${d.BRK_NAME}, no relevant international human rights complaint mechanisms are available for (rejected) asylum seekers. If you still wish to take initiative in the context, please assess the further possibilities applicable to all countries listed below. </p>`
+                      : UNTreatyBody
+                  }                  
+                  ${d.regionalHumanRightsMechanism[0]?.length === 0 ? `<h4>Regional Human Rights Mechanism:</h4><p>Unfortunately in ${d.BRK_NAME}, no Regional Human Rights Mechanism are available for (rejected) asylum seekers.</p>` : RegionalHuman}
+                </div>`);
+          });
+      })
+
+      .onPolygonHover((hoverD) =>
+        world
+          .polygonAltitude((d) => (d === hoverD ? 0.12 : 0.06))
+          .polygonCapColor((d) => {
+            // return d === hoverD ? "white" : d?.properties?.color;
+          })
+      )
+      .polygonsTransitionDuration(300)(document.getElementById("globeViz"));
+  });
--- index.js
+++ index.js
@@ -1,57 +1,17 @@
-// Create a Three.js scene and camera
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const popup = document.querySelector("#popup-container");
 
-// Create a renderer
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-// Load the globe texture
-var globeTexture = new THREE.TextureLoader().load( 'globe.jpg' );
-
-// Create a sphere geometry and apply the globe texture
-var globeGeometry = new THREE.SphereGeometry( 5, 32, 32 );
-var globeMaterial = new THREE.MeshBasicMaterial( { map: globeTexture } );
-var globe = new THREE.Mesh( globeGeometry, globeMaterial );
-scene.add( globe );
-
-// Load the flag textures
-var flagTextures = {};
-flagTextures['us'] = new THREE.TextureLoader().load( 'us.jpg' );
-flagTextures['uk'] = new THREE.TextureLoader().load( 'uk.jpg' );
-// ... add more flag textures as needed
-
-// Create a map of country codes to information
-var countryInfo = {};
-countryInfo['us'] = { name: 'United States', capital: 'Washington D.C.', population: 328200000 };
-countryInfo['uk'] = { name: 'United Kingdom', capital: 'London', population: 67000000 };
-// ... add more country information as needed
-
-// Create flag meshes for each country and add them to the scene
-var flags = {};
-for (var countryCode in flagTextures) {
-  var flagGeometry = new THREE.PlaneGeometry( 1, 1 );
-  var flagMaterial = new THREE.MeshBasicMaterial( { map: flagTextures[countryCode] } );
-  var flag = new THREE.Mesh( flagGeometry, flagMaterial );
-  flag.position.set( /* calculate position on globe */ );
-  scene.add( flag );
-  flags[countryCode] = flag;
+function showPopup(data) {
+  popup.innerHTML = data;
+  popup.style.display = "block";
 }
 
-// Add event listeners to the flags
-for (var countryCode in flags) {
-  flags[countryCode].addEventListener( 'click', function() {
-    var info = countryInfo[countryCode];
-    // Display the information on the screen
-  });
+function hidePopup() {
+  popup.style.display = "none";
+  document.removeEventListener("click", hidePopupOnClickOutside);
 }
 
-// Render the scene
-function animate() {
-  requestAnimationFrame( animate );
-  renderer.render( scene, camera );
+function hidePopupOnClickOutside(event) {
+  if (!popup.contains(event.target)) {
+    hidePopup();
+  }
 }
-animate();
-
-// Uncaught (in promise) TypeError: Cannot read properties of undefined (reading 'length')
